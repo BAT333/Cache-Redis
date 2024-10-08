e449cd82c1a3e98e1a2607d6d5fe4827efc29303
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-10-08 19:47:33 -0300

    Redis

--- src/main/java/com/cache/demo/model/DataEmpresa.java
+++ src/main/java/com/cache/demo/model/DataEmpresa.java
@@ -2,10 +2,12 @@ package com.cache.demo.model;
 
 import com.cache.demo.domain.Empresa;
 
+import java.io.Serializable;
+
 public record DataEmpresa(
         Long id,
         String name
-) {
+)implements Serializable {
     public DataEmpresa(Empresa empresa){
         this(empresa.getId(), empresa.getNome());
     }
