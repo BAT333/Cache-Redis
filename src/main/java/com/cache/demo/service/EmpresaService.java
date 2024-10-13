76058f53c34a9eb9922ada6310dd90bb8647547a
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-10-13 16:18:29 -0300

    log

--- src/main/java/com/cache/demo/service/EmpresaService.java
+++ src/main/java/com/cache/demo/service/EmpresaService.java
@@ -3,6 +3,7 @@ package com.cache.demo.service;
 import com.cache.demo.domain.Empresa;
 import com.cache.demo.model.DataEmpresa;
 import com.cache.demo.repository.RepositoryEmpresa;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +11,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class EmpresaService {
     @Autowired
     private RepositoryEmpresa empresa;
 
     public List<DataEmpresa> all() {
+        log.info("FAZENDO PESQUISA SEM CACHE");
         return empresa.findAll().stream().map(DataEmpresa::new).collect(Collectors.toList());
     }
 
     public void add() {
-        for (int i =0;i<=300000;i++){
-            empresa.save(new Empresa(Integer.toUnsignedLong(i),"a"+i));
-        }
+        log.info("ADD MAIS EMPRESA");
     }
 
     public DataEmpresa update(Long id) {
+        log.info("ATUALIZANDO");
         var e =  this.empresa.findById(id);
         return e.map(DataEmpresa::new).get();
     }
